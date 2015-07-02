15f51e84552196843e1b0313b2f5bd81e8e6aafd
Bassel-Bakr <basselbakr@gmail.com.com>
2015-07-02 09:18:37 +0200

    Optimize Converter

--- src/com/bassel/cmd/Convert.java
+++ src/com/bassel/cmd/Convert.java
@@ -21,53 +21,53 @@ import java.util.List;
 import java.util.concurrent.Future;
 
 public class Convert {
-	public synchronized static ShellResult future2present(Future<ShellResult> future) {
-		try {
-			return future.get();
-		} catch (Exception e) {Debug.log(e);
-			return null;}
-	}
-
-	public synchronized static List<String> array2list(String[] array) {
-		return Arrays.asList(array);
-	}
-
-	public synchronized static String array2string(String[] array) {
-		StringBuilder string = new StringBuilder();
-		for (String line : array) {
-			string.append(line + "\n");
+		public synchronized static ShellResult future2present(Future<ShellResult> future) {
+				try {
+						return future.get();
+				} catch (Exception e) {Debug.log(e);
+						return null;}
 		}
-		if (string.length() > 1)
-			string.deleteCharAt(string.length());
-		return string.toString();
-	}
 
-	public synchronized static String[] list2array(List<String> list) {
-		return list.toArray(new String[list.size()]);
-	}
-
-	public synchronized static String list2string(List<String> list) {
-		StringBuilder string = new StringBuilder();
-		for (String line : list) {
-			string.append(line + "\n");
+		public synchronized static List<String> array2list(String[] array) {
+				return Arrays.asList(array);
 		}
-		if (string.length() > 1)
-			string.deleteCharAt(string.length());
-		return string.toString();
-	}
 
-	public synchronized static String[] string2array(String string) {
-		return string.split("\n");
-	}
+		public synchronized static String array2string(String[] array) {
+				StringBuilder string = new StringBuilder();
+				int size = array.length;
+				for (int i = 0; i < size; i++)
+						string.append(array[i] + "\n");
+				if (string.length() > 1)
+						string.deleteCharAt(string.length());
+				return string.toString();
+		}
 
-	public synchronized static List<String> string2list(String string) {
-		return array2list(string.split("\n"));
-	}
+		public synchronized static String[] list2array(List<String> list) {
+				return list.toArray(new String[list.size()]);
+		}
 
-	public synchronized static String trimString(StringBuilder string) {
-		int len = string.length();
-		if (len > 1)
-			string.deleteCharAt(len - 1);
-		return string.toString();
-	}
+		public synchronized static String list2string(List<String> list) {
+				StringBuilder string = new StringBuilder();
+				int size = list.size();
+				for (int i = 0; i < size; i++)
+						string.append(list.get(i) + "\n");
+				if (string.length() > 1)
+						string.deleteCharAt(string.length());
+				return string.toString();
+		}
+
+		public synchronized static String[] string2array(String string) {
+				return string.split("\n");
+		}
+
+		public synchronized static List<String> string2list(String string) {
+				return array2list(string.split("\n"));
+		}
+
+		public synchronized static String trimString(StringBuilder string) {
+				int len = string.length();
+				if (len > 1)
+						string.deleteCharAt(len - 1);
+				return string.toString();
+		}
 }
