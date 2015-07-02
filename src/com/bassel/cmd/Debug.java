c656c20f7d4790bb5a53c02e0c2f91cee6cc3ed7
Bassel-Bakr <basselbakr@gmail.com.com>
2015-07-02 08:55:47 +0200

    Add loggers

--- src/com/bassel/cmd/Debug.java
+++ src/com/bassel/cmd/Debug.java
@@ -16,47 +16,57 @@
 
 package com.bassel.cmd;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Debug {
-	private static boolean DEBUG;
+		private static boolean DEBUG;
+		private static Logger logger;
 
-	public static boolean debug() {
-		return DEBUG;
-	}
+		static {
+				DEBUG = true;
+				logger = Logger.getLogger("Cmd");
+		}
 
-	public static void debug(boolean DEBUG) {
-		Debug.DEBUG = DEBUG;
-	}
+		public static boolean debug() {
+				return DEBUG;
+		}
 
-	public static void log(String command) {
-		if (!DEBUG) return;
-		//Log.d("Cmd_Shell", command);
-	}
+		public static void debug(boolean DEBUG) {
+				Debug.DEBUG = DEBUG;
+		}
 
-	public static void log(String TAG, String command) {
-		if (!DEBUG) return;
-		//Log.d(TAG, command);
-	}
+		public static void log(String command) {
+				if (!DEBUG) return;
+				logger.log(Level.WARNING, command);
+		}
 
-	public static void log(String[] commands) {
-		if (!DEBUG) return;
-		//for (String command : commands)
-			//Log.d("Cmd_Shell", command);
-	}
+		public static void log(String TAG, String command) {
+				if (!DEBUG) return;
+				logger.log(Level.WARNING, command);
+		}
 
-	public static void log(List<String> commands) {
-		if (!DEBUG) return;
-		//for (String command : commands)
-			//Log.d("Cmd_Shell", command);
-	}
+		public static void log(String[] commands) {
+				if (!DEBUG) return;
+				int size = commands.length;
+				for (int i = 0; i < size; i++)
+						logger.log(Level.WARNING, commands[i]);
+		}
 
-	public static void log(Throwable t) {
-		if (!DEBUG) return;
-		//Log.d("Cmd_Shell", t.getMessage(), t);
-	}
+		public static void log(List<String> commands) {
+				if (!DEBUG) return;
+				int size = commands.size();
+				for (int i = 0; i < size; i++)
+						logger.log(Level.WARNING, commands.get(i));
+		}
 
-	public static void log(String TAG, Throwable t) {
-		if (!DEBUG) return;
-		//Log.d(TAG, t.getMessage(), t);
-	}
+		public static void log(Throwable t) {
+				if (!DEBUG) return;
+				logger.log(Level.WARNING, t.getMessage(), t);
+		}
+
+		public static void log(String TAG, Throwable t) {
+				if (!DEBUG) return;
+				Logger.getLogger(TAG).log(Level.WARNING, t.getMessage(), t);
+		}
 }
